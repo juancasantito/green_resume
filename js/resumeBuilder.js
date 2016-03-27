1fb12b06293a239b50b0a92a07c9deacb534f248
kervin5 <kervinlvh@gmail.com>
2016-03-26 20:44:44 -0700

    Test .append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,10 @@
 $("#main").append("Kervin Vasquez");
+
+var awesomeThoughts = "I am Kervin and I am AWESOME!";
+
+funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+$("#main").append(funThoughts);
